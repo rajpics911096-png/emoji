14443093e7b78d7f3605c9bca92d4dc03978eef9
raju kumar <rajpics911096@gmail.com>
2025-12-03 02:47:28 +0000

    Login ke related aur koi security add karna chahte hain to Karen taki we

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 
 import type {NextConfig} from 'next';
 
+const contentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.google.com;
+  child-src 'self';
+  style-src 'self' 'unsafe-inline' fonts.googleapis.com;
+  img-src * 'self' data:;
+  media-src 'self';
+  connect-src *;
+  font-src 'self' fonts.gstatic.com;
+`;
+
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -30,6 +41,19 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: contentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ]
   },
 };
 
