b423484e8290706001bf3cf51da6d56f53af5657
raju kumar <rajpics911096@gmail.com>
2025-12-03 09:01:22 +0000

    Dashboard mein header aur footer show kar raha hai use remove Karen

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -7,24 +7,25 @@ import type { ReactNode } from 'react';
 import { i18n } from '@/lib/i18n-config';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
-import { useParams } from 'next/navigation';
+import { useParams, usePathname } from 'next/navigation';
 
 export default function RootLayout({
   children,
-  params,
 }: Readonly<{
   children: React.ReactNode;
-  params: { lang: string };
 }>) {
   const paramsFromHook = useParams<{ lang: string }>();
+  const pathname = usePathname();
   const lang = i18n.locales.includes(paramsFromHook.lang as any) ? paramsFromHook.lang : i18n.defaultLocale;
+  
+  const isAdminPage = pathname.includes('/rajurajadmin');
 
   return (
     <ClientProviders lang={lang}>
         <div className="flex flex-col min-h-screen">
-            <Header lang={lang} />
+            {!isAdminPage && <Header lang={lang} />}
             <div className="flex-1">{children}</div>
-            <Footer lang={lang} />
+            {!isAdminPage && <Footer lang={lang} />}
         </div>
         <Toaster />
     </ClientProviders>
