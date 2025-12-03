33c883a6d684c38aefc91cb6838d95e551c52568
raju kumar <rajpics911096@gmail.com>
2025-12-03 08:00:29 +0000

    Dashboard mein header  section remove Karen ise admin panel ke andar Koi

--- src/app/[lang]/rajurajadmin/layout.tsx
+++ src/app/[lang]/rajurajadmin/layout.tsx
@@ -2,9 +2,12 @@
 import { AuthGuard } from "@/components/auth-guard";
 import type { ReactNode } from 'react';
 import { ClientProviders } from "@/components/client-providers-admin";
+import '@/app/globals.css';
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
 
-export default function AdminLayout({
+export default function AdminRootLayout({
   children,
   params
 }: {
@@ -12,10 +15,33 @@ export default function AdminLayout({
   params: { lang: string };
 }) {
   return (
-    <ClientProviders lang={params.lang}>
-        <AuthGuard>
-            {children}
-        </AuthGuard>
-    </ClientProviders>
+    <html lang={params.lang} suppressHydrationWarning>
+        <head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+            />
+            <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+            />
+        </head>
+      <body className={cn('font-body antialiased min-h-screen flex flex-col')}>
+        <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+        >
+            <ClientProviders lang={params.lang}>
+                <AuthGuard>
+                    {children}
+                </AuthGuard>
+            </ClientProviders>
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
